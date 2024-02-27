eaa1913184411737e8059e728968765d1cfd55c9
Onwuka2007 <158163435+Onwuka2007@users.noreply.github.com>
2024-02-27 22:30:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//setting date here
+var countDownDate = new Date("Mar 23, 2024 22:58:59").getTime();
 
+  var countdownfunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.querySelector(".countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
